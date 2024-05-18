33f2e73486c8b830a862284d76c75a2559e04c8d
Arcandres <Arcandres@hotmail.com>
2024-05-18 14:12:56 -0500

    Refactoring code to be able to play without refreshing main page

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-var n1 = Math.floor(Math.random()*6)+1;
-var n2 = Math.floor(Math.random()*6)+1;
-console.log(n1);
-console.log(n2);
-function insertImage1(x) {
-    document.querySelector("img.img"+ x).setAttribute("src", "images/dice" + n1 + ".png");
+// Let's store these HTML elements
+let button = document.querySelector('.play');
+let heading = document.querySelector("h1");
+let img1 = document.querySelector(".img1");
+let img2 = document.querySelector(".img2");
+
+// Creating a function to get a random number each time its called
+function number() {
+    return Math.floor(Math.random() * 6) + 1;
 }
 
-function insertImage2(x) {
-    document.querySelector("img.img"+ x).setAttribute("src", "images/dice" + n2 + ".png");
+function getDiceNumbers() {
+    const n1 = number(); // getting a random number
+    const n2 = number(); // we get a different number even if we call the same function again
+
+    img1.setAttribute("src", "images/dice" + n1 + ".png");
+    img2.setAttribute("src", "images/dice" + n2 + ".png");
+
+    // We need to return the value of the random numbers
+    // so we can compare them below
+    return { n1, n2 }
 }
 
 function getWinner() {
-    if(n1>n2) {
-        document.querySelector("h1").innerText = "🚩Player 1 won!";
-    } 
-    else if(n2>n1) {
-        document.querySelector("h1").innerText = "Player 2 won!🚩";
+    // Getting values from the previous function
+    const { n1, n2 } = getDiceNumbers();
+
+    if (n1 > n2) {
+        heading.innerText = "🚩Player 1 won!";
+    }
+    else if (n2 > n1) {
+        heading.innerText = "Player 2 won!🚩";
     } else {
-        document.querySelector("h1").innerText = "Draw!";
+        heading.innerText = "Draw!";
     }
 }
 
-insertImage1(1);
-insertImage2(2);
-getWinner();
+button.addEventListener('click', getWinner)
